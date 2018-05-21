1cd996ffca79bbd3bdf3c6ccdb346879fb05ed7b
alex olmos <32845733+bankoksu@users.noreply.github.com>
2018-05-21 18:26:56 +0200

    actualizado 21/05
    terminar el resto

--- proyecto_LLMM-master/proyecto con Json y scroll infinito/js/noticias.js
+++ proyecto_LLMM-master/proyecto con Json y scroll infinito/js/noticias.js
@@ -6,8 +6,40 @@ window.onload =function(){
 	});
 
 	function pintar(json){
-	 $.each( json, function( i, empleado ) {
+	 $.each( json, function(i,j) {
  	 $("#out").append( "<li id='" + i + "'>" + empleado.firstName+ " " + empleado.lastName + "</li>" );
+ 	console.log(i);
+	console.log(j);
  	}); 
 	}
 }
+/*
+cosas que me explico juanan :P
+$(json).each(function(k,v){
+	console.log(k);
+	console.log(v);
+})
+
+$("h1").appendTo("#id")
+
+$("h1")  = // get elements by tag name
+$(".class") = // get elements by class name
+$("#id")
+
+$.ajax( {
+
+});
+
+.appentTo
+
+.class
+
+.attr
+
+var Titutlo = gel
+
+$(titulo).appençto
+
+.0append()
+
+*/
