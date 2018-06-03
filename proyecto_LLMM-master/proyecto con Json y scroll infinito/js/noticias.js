ae92c2d811a645e8db92ed8ff7ee5e0d54bb9d1c
alex olmos <32845733+bankoksu@users.noreply.github.com>
2018-06-03 14:15:20 +0200

    en general cambios en la pagina principal

--- proyecto_LLMM-master/proyecto con Json y scroll infinito/js/noticias.js
+++ proyecto_LLMM-master/proyecto con Json y scroll infinito/js/noticias.js
@@ -1,17 +1,46 @@
 window.onload =function(){
 
-	$.getJSON( "noticias.json", function( jsonObject ) {
+	/*$.getJSON( "../noticias2.json", function( jsonObject ) {
   	//en aquest punt l'objecte jsonObject correspon al fitxer
   	pintar( jsonObject );
 	});
-
+	var id = 1 
 	function pintar(json){
-	 $.each( json, function(i,j) {
- 	 $("#out").append( "<li id='" + i + "'>" + empleado.firstName+ " " + empleado.lastName + "</li>" );
+	 $.each( json, function(titulo,texto) {
+ 	 $("#out").append( "<li id='" + id + "'>" + noticias.titulo+ " " + noticas.texto + "</li>" );
  	console.log(i);
 	console.log(j);
  	}); 
-	}
+	}*/
+
+
+	$(function(){
+    //cargar comunidades
+    $.getJSON( "../noticias2.json", function( jsonObject ) {
+        ponerComunidades( jsonObject );
+    });
+
+    //onclick en verinfo
+    /*$("#verinfo").click(function() {
+        var v=$("#comunidades").val();
+        if (v!=0) window.location = "/comunidad/"+$("#comunidades").val();
+        else alert('Selecciona una comunidad');
+    });*/
+});
+
+function ponerComunidades(json){
+     $.each( json, function( i, comunidad ) {
+     	console.log(i);
+     	console.log(j);
+         //dos formas de hacer lo mismo, la segunda es más adecuada.
+         //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
+         $('#agregador').append($("<h1>" + i + "</h1>","<p>" + comunidad +"</p>"));
+         //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
+     }); 
+}
+
+
+
 }
 /*
 cosas que me explico juanan :P
