ec497e2b05243c39bae198a870739fc7d9ba3b4b
alex olmos <32845733+bankoksu@users.noreply.github.com>
2018-06-15 04:27:51 +0200

    el scroll funciona :) + función para pararlo

--- proyecto_LLMM-master/proyecto con Json y scroll infinito/js/noticias.js
+++ proyecto_LLMM-master/proyecto con Json y scroll infinito/js/noticias.js
@@ -1,28 +1,66 @@
 window.onload =function(){
-	/*var doc = [
-{ 
-"titulo":"contenido de la misma alsdfagfgfdgfdgfdsasdf",
-"texto":"primera noticia con su titulo"
 
-},
-{
-"titulo":"asdfasfasdsdadafasdfasdjnhsfgjjukikfykujk",
-"texto":"segunda noticia con su titulo"
-
-},
-{
-"titulo":"asdasdasdasdadsasd",
-"texto":"pruebas pruebas"
-
-}
-
-]*/
-	/*console.log(doc);*/
-	/*esta parte crea la seccion del scroll infinito */
-(function(){
+  var contador = 0;
+  var stop = true;
+  var final = false;
+  (function(){
   $(window).scroll(function() {
-  	var contador = 0
-  	var contador2 =0
+    // How much the user has scrolled
+    const scrollTop = $(window).scrollTop();
+    // windowHeigth is the height of the window
+    const windowHeight = $(window).height();
+    // documentHeight could be larger than windowHeight
+    const documentHeight = $(document).height();
+    const offset = 10;
+    // add windowHeight is neccessary to get to the bottom
+    if(scrollTop + windowHeight > documentHeight - offset && stop == true) {
+      console.log("estoy en la funcion scroll infinito");  
+      $.getJSON( "./noticias2.json", function( jsonObject ) {
+      //en aquest punt l'objecte jsonObject correspon al fitxer
+      console.log(jsonObject);
+      pintar( jsonObject );
+
+  });
+
+    function pintar(json){
+    $.each( json, function( i, empleado ) {
+      console.log(i);
+      contador =+ 1;
+      stop = false;
+      final = true;
+      console.log(contador);
+      /*$("#agregador").append("<div class='jumbotron text-center' id='publi'><h2>No quedan mas noticias para mostrar vuelve más tarde :P</h2></div>");*/
+      $("#agregador").append("<h2>"+empleado.titulo+"<h2>");
+      $("#agregador").append("<p>"+empleado.fecha+"</p>");
+      $("#agregador").append("<img class='img-fluid float-left img-thumbnail mr-4' src='" + empleado.img + "'>");
+      $("#agregador").append("<p>"+empleado.texto+"</p>");
+
+      if (contador > 4){
+          $("#agregador").append("<p> no tengo más noticias</p>");
+          $("#agregador").append("<p>:P vuelve mas tarde</p>");
+      }
+      /*$("#agregador").append("<div class='jumbotron text-center'" +" id='publi'>" +"<h2>"+empleado.titulo+ "</h2>" +"<img src='"+empleado.img+"'"+" class='imagen' alt="""">"+"<p>"+empleado.fecha+"</p>"+"<p>"+ empleado.texto+"</p>"+"</div>");*/
+  }); 
+  }
+
+    }
+    if(scrollTop + windowHeight > documentHeight - offset && final == true) {
+      $("#agregador2").append("<h2>No tenemos mas noticias por el momento</h2>");
+      /*$("#agregador2").append("<img class='img-fluid float-left img-thumbnail mr-4' src='nonoticia.jpg'>");*/
+      $("#agregador2").append("<p>vuelve en otro momento para más actualidad del mundo natural </p>");
+      $("#agregador2").append("<h2>Te esperamos, recuerda seguirnos en nuestra redes sociales</h2>")
+      final = false;
+    }
+
+  });
+}());
+
+
+
+}  // Si la página no es la página principal la función parará aqui por lo que no cargará las entradillas.
+/*(function(){
+  $(window).scroll(function() {
+
 
     const scrollTop = $(window).scrollTop();
 
@@ -36,44 +74,23 @@ window.onload =function(){
       const divs = $('div').length;
       $('#agregador').append(() => {
         let result = '';
-        var textoparse = doc
-        /*if (contador > 3) {
-        	if (contador2 > 0){
-        		result = ""
-        	}else{
-        		result += `<div>no quedan mas noticias para mostrar</div>`
-        		contador2 += 1
-        	}
-        } */
-        for (var i = 0; i< doc.length; i++){
-        	result += ('<h2>' + textoparse[i].titulo + '</h2>' +'<p>' +textoparse[i].texto +'</p>')
-        }
-        /*for (let i = divs + 1; i <= divs + 1; i++) {
-          result += `<div>Content ${i}</div>`
-          contador += 1*/
-          /*if (contador = 3){
-          	if(contador2 >){
-          	 	return ""
-          	}else{
-          		result += `<div>no quedan mas noticias para mostrar</div>`
-        		contador2 += 1
-          	}
-          }*/
-       /* }*/
-        return result;
-      });
+        console.log("estoy antes de leer el json");
+        var noticias = $.getJSON( "../noticias2.json")
+        console.log(jsonObject);
+        $.each( noticias, function(i,texto)
+
+        });
     }
-  });
+  )};
 }());
-	     /*   console.log("estoy antes de leer el json");
-		 	$.getJSON( "../noticias2.json"), function( jsonObject ) {
+ */
 		  	//en aquest punt l'objecte jsonObject correspon al fitxer
-		  	console.log(jsonObject);
+		  /*	
 		  	pintar( jsonObject );
 			};
 
 			function pintar(json){
-			 $.each( json, function(i,texto) {
+			  {
 			 console.log("estoy donde se supone que pinta")
 			 console.log(titulo);
 			 console.log(texto);
@@ -82,22 +99,6 @@ window.onload =function(){
 			console.log(j);
 		 	}); 
 			}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
 /*
 	$(function(){
     //cargar comunidades
